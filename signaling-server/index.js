849be7f89a93347e340788e1a59dd1b7056180df
Greg Field <greg.field@clio.com>
2021-02-11 10:57:56 -0800

    First stab at game websocket server

--- signaling-server/index.js
+++ signaling-server/index.js
@@ -1,152 +1,214 @@
 //require our websocket library
-var WebSocketServer = require('ws').Server;
+var WebSocketServer = require("ws").Server;
 
 //creating a websocket server at port 9090
-var wss = new WebSocketServer({port: 9090});
+var wss = new WebSocketServer({ port: 9090 });
 
 //all connected to the server users
-var users = {};
+var games = {};
 
 //when a user connects to our sever
-wss.on('connection', function(connection) {
+wss.on("connection", function (connection) {
+  console.log("User connected");
 
-   console.log("User connected");
+  //when server gets a message from a connected user
+  connection.on("message", function (message) {
+    var data;
+    //accepting only JSON messages
+    try {
+      data = JSON.parse(message);
+      console.log(`Received message of type ${data.type}`);
+    } catch (e) {
+      console.log("Invalid JSON");
+      data = {};
+    }
 
-   //when server gets a message from a connected user
-   connection.on('message', function(message) {
+    //switching type of the user message
+    switch (data.type) {
+      case "join":
+        game = games[data.gameCode];
 
-      var data;
-      //accepting only JSON messages
-      try {
-         data = JSON.parse(message);
-      } catch (e) {
-         console.log("Invalid JSON");
-         data = {};
-      }
+        if (game) {
+          if (game.players.length === 1 && !game.players[data.name]) {
+            console.log("Joining game: ", data.gameCode);
 
-      //switching type of the user message
-      switch (data.type) {
-         //when a user tries to login
+            game.players[data.name] = connection;
+            connection.gameCode = data.gameCode;
 
-         case "login":
-            console.log("User logged", data.name);
-
-            //if anyone is logged in with this username then refuse
-            if(users[data.name]) {
-               sendTo(connection, {
-                  type: "login",
-                  success: false
-               });
-            } else {
-               //save user connection on the server
-               users[data.name] = connection;
-               connection.name = data.name;
-
-               sendTo(connection, {
-                  type: "login",
-                  success: true
-               });
-            }
-
-            break;
-
-         case "offer":
-            //for ex. UserA wants to call UserB
-            console.log("Sending offer to: ", data.name);
-
-            //if UserB exists then send him offer details
-            var conn = users[data.name];
-
-            if(conn != null) {
-               //setting that UserA connected with UserB
-               connection.otherName = data.name;
-
-               sendTo(conn, {
-                  type: "offer",
-                  offer: data.offer,
-                  name: connection.name
-               });
-            }
-
-            break;
-
-         case "answer":
-            console.log("Sending answer to: ", data.name);
-            //for ex. UserB answers UserA
-            var conn = users[data.name];
-
-            if(conn != null) {
-               connection.otherName = data.name;
-               sendTo(conn, {
-                  type: "answer",
-                  answer: data.answer
-               });
-            }
-
-            break;
-
-         case "candidate":
-            console.log("Sending candidate to:",data.name);
-            var conn = users[data.name];
-
-            if(conn != null) {
-               sendTo(conn, {
-                  type: "candidate",
-                  candidate: data.candidate
-               });
-            }
-
-            break;
-
-         case "leave":
-            console.log("Disconnecting from", data.name);
-            var conn = users[data.name];
-            conn.otherName = null;
-
-            //notify the other user so he can disconnect his peer connection
-            if(conn != null) {
-               sendTo(conn, {
-                  type: "leave"
-               });
-            }
-
-            break;
-
-         default:
             sendTo(connection, {
-               type: "error",
-               message: "Command not found: " + data.type
+              type: "joined",
+              success: true,
             });
-
-            break;
-      }
-   });
-
-   //when user exits, for example closes a browser window
-   //this may help if we are still in "offer","answer" or "candidate" state
-   connection.on("close", function() {
-
-      if(connection.name) {
-      delete users[connection.name];
-
-         if(connection.otherName) {
-            console.log("Disconnecting from ", connection.otherName);
-            var conn = users[connection.otherName];
-            conn.otherName = null;
-
-            if(conn != null) {
-               sendTo(conn, {
-                  type: "leave"
-               });
+          } else {
+            if (game.players.length !== 1) {
+              console.log("Join failed due to game full: ", data.gameCode);
+            } else {
+              console.log("Join failed due to name taken: ", data.gameCode);
             }
-         }
+
+            sendTo(connection, {
+              type: "joined",
+              success: false,
+            });
+          }
+        } else {
+          console.log("Game created: ", data.gameCode);
+
+          game = { players: {} };
+          game.players[data.name] = connection;
+          connection.gameCode = data.gameCode;
+
+          sendTo(connection, {
+            type: "created",
+            success: true,
+          });
+        }
+
+        break;
+
+      case "offer":
+        var game = games[data.gameCode];
+
+        if (game && game.players.length === 2) {
+          console.log(
+            `Sending offer from player ${data.name} for ${data.gameCode}`
+          );
+
+          otherConnection = otherPlayerConnection(game, data.name);
+
+          sendTo(otherConnection, {
+            type: "offer",
+            offer: data.offer,
+          });
+        } else {
+          if (!game) {
+            console.log("Game doesn't exist: ", data.gameCode);
+          } else {
+            console.log("Game doesn't have another player: ", data.gameCode);
+          }
+        }
+
+        break;
+
+      case "answer":
+        var game = game[data.gameCode];
+
+        if (game && game.players === 2) {
+          console.log(`Sending answer from ${data.name} for ${data.gameCode}`);
+
+          otherConnection = otherPlayerConnection(game, data.name);
+
+          sendTo(otherConnection, {
+            type: "answer",
+            answer: data.answer,
+          });
+        } else {
+          if (!game) {
+            console.log("Game doesn't exist: ", data.gameCode);
+          } else {
+            console.log("Game doesn't have another player: ", data.gameCode);
+          }
+        }
+
+        break;
+
+      case "candidate":
+        var game = game[data.gameCode];
+
+        if (game && game.players === 2) {
+          console.log(
+            `Sending candidate from ${data.name} for ${data.gameCode}`
+          );
+
+          otherConnection = otherPlayerConnection(game, data.name);
+
+          sendTo(otherConnection, {
+            type: "candidate",
+            candidate: data.candidate,
+          });
+        } else {
+          if (!game) {
+            console.log("Game doesn't exist: ", data.gameCode);
+          } else {
+            console.log("Game doesn't have another player: ", data.gameCode);
+          }
+        }
+
+        break;
+
+      case "leave":
+        var game = game[data.gameCode];
+
+        if (game && game.players === 2) {
+          console.log(`Player ${data.name} is leaving game ${data.gameCode}`);
+
+          otherConnection = otherPlayerConnection(game, data.name);
+
+          sendTo(otherConnection, {
+            type: "leave",
+          });
+        } else {
+          if (!game) {
+            console.log("Game doesn't exist: ", data.gameCode);
+          } else {
+            console.log("Game doesn't have another player: ", data.gameCode);
+          }
+        }
+
+        break;
+
+      default:
+        sendTo(connection, {
+          type: "error",
+          message: "Command not found: " + data.type,
+        });
+
+        break;
+    }
+  });
+
+  //when user exits, for example closes a browser window
+  //this may help if we are still in "offer","answer" or "candidate" state
+  connection.on("close", function () {
+    console.log("Hanlding connection closed");
+
+    if (connection.gameCode) {
+      const gameCode = connection.gameCode;
+      console.log(`Connection closed on ${gameCode}`);
+
+      const game = games[gameCode]
+      const playerName = Object.keys(game.players).find(name => {
+        return game.players[name] === connection;
+      })
+      delete game.players[playerName]
+
+      if (Object.keys(game.players).length === 0) {
+        console.log(`Deleting game ${gameCode}`);
+
+        delete games[gameCode];
+      } else {
+        console.log(`Notifying other player ${gameCode}`);
+
+        const otherConnection = otherPlayerConnection(game, playerName)
+
+        sendTo(otherConnection, {
+          type: "leave",
+        });
       }
-   });
+    }
+  });
 
-   connection.send("Hello world");
-
+  connection.send("Hello world");
 });
 
+function otherPlayerConnection(game, playerName) {
+  const otherPlayerName = Object.keys(game.players).find((name) => {
+    return name !== playerName;
+  });
+
+  return game.players[otherPlayerName];
+}
+
 function sendTo(connection, message) {
-   connection.send(JSON.stringify(message));
+  connection.send(JSON.stringify(message));
 }
