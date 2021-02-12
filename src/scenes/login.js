ca8ea32055091141545e1ae65f0438b2161191e5
Robin Phung <robin.phung@clio.com>
2021-02-12 00:49:04 -0800

    remove login skip

--- src/scenes/login.js
+++ src/scenes/login.js
@@ -10,8 +10,6 @@ export default class Login extends Phaser.Scene {
   };
 
   create = () => {
-    this.scene.start("Game");
-
     this.element = this.add.dom(640, 600).createFromCache("loginForm");
     window.element = this.element;
 
